ba66685aca834b4dbc6500b79b979b796ca25381
disini <disini@live.cn>
2020-12-08 14:15:56 +0800

    add 3rdparty lib resource files into projects

--- external/assimp/revision.h
+++ external/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x1243b18a
+#define GitVersion 0x026a380e
 #define GitBranch "main"
 
 #endif // ASSIMP_REVISION_H_INC
